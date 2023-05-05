5a196cab07821fd7a3a2381b846e99feb4df9e20
AzureandGit <48793747+AzureandGit@users.noreply.github.com>
2023-05-05 08:33:36 +0300

    Create index.js

---
+++ index.js
@@ -0,0 +1,13 @@
+# This is my simple Hello World app
+
+const http = require('http');
+
+const server = http.createServer((req, res) => {
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/plain');
+  res.end('Hello, World!');
+});
+
+server.listen(3000, () => {
+  console.log('Server running on http://localhost:3000/');
+});
